995eae6c7d10ab7cb2d1a6a664dab7906f6d8644
jarlenandres <jaarojas@gmai.com>
2025-10-10 13:07:51 -0500

    Code updating LinkedList

---
+++ LinkedList/LinkedList.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using LinkedList.Core;
+using System.ComponentModel.Design;
+
+var list = new SinglyLinkedList<string>();
+var opc = string.Empty;
+
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "0":
+            Console.WriteLine("See you next time...");
+            break;
+        case "1":
+            Console.Write("Enter the value: ");
+            list.InsertAtBeginning(Console.ReadLine()!);
+            break;
+        case "2":
+            Console.Write("Enter the value: ");
+            list.InsertAtEnd(Console.ReadLine()!);
+            break;
+        case "3":
+            list.PrintList();
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+} while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("1. Insert at biginning.");
+    Console.WriteLine("2. Insert at end.");
+    Console.WriteLine("3. Print the list.");
+    Console.WriteLine("0. Exit.");
+    Console.Write("Enter your option: ");
+    return Console.ReadLine()!;
+}
