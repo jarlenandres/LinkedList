5280645980fcfb122c7b41bcec18962ec7bdaade
jarlenandres <jaarojas@gmai.com>
2025-10-14 15:35:09 -0500

    Creating doubly linked list project

---
+++ LinkedList/DoubleList.UI.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using DoubleList;
+
+var list = new DoubleLinkedList<string>();
+var opc = string.Empty;
+
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "0":
+            Console.WriteLine("See you next time...");
+            break;
+        case "1":
+            Console.Write("Enter the value: ");
+            list.InsertAtBeginning(Console.ReadLine()!);
+            break;
+        case "2":
+            Console.Write("Enter the value: ");
+            list.InsertAtEnd(Console.ReadLine()!);
+            break;
+        //case "3":
+        //    list.PrintList();
+        //    break;
+        //case "4":
+        //    Console.Write("Enter the value: ");
+        //    Console.Write($"The result is {list.Contains(Console.ReadLine()!)}");
+        //    break;
+        //case "5":
+        //    list.Remove(Console.ReadLine()!);
+        //    break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+
+} while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("1. Insert at biginning.");
+    Console.WriteLine("2. Insert at end.");
+    Console.WriteLine("3. Print the list.");
+    Console.WriteLine("4. Search in the list.");
+    Console.WriteLine("5. Remove the first in the list.");
+    Console.WriteLine("0. Exit.");
+    Console.Write("Enter your option: ");
+    return Console.ReadLine()!;
+}
---
+++ LinkedList/DoubleList/DoubleLinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList
+{
+    public class DoubleLinkedList<T>
+    {
+        private DoubleNode<T>? _head;
+        private DoubleNode<T>? _tail;
+
+        public DoubleLinkedList()
+        {
+            _tail = null;
+            _head = null;
+        }
+
+        public void InsertAtBeginning(T data)
+        {
+            var newNode = new DoubleNode<T>(data);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                newNode.Next = _head;
+                _head.Prev = newNode;
+                _head = newNode;
+            }
+        }
+
+        public void InsertAtEnd(T data)
+        {
+            var newNode = new DoubleNode<T>(data);
+            if (_tail == null)
+            {
+                _tail = newNode;
+                _head = newNode;
+            }
+            else
+            {
+                newNode.Prev = _tail;
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+        }
+
+        public string GetForward()
+        {
+            var outpot = string.Empty;
+            var current = _head;
+            while (current != null)
+            {
+                outpot += $"{current.Data} <=> ";
+                current = current.Next;
+            }
+            return outpot;
+        }
+
+        public string GetBackward()
+        {
+            var outpot = string.Empty;
+            var current = _tail;
+            while (current != null)
+            {
+                outpot += $"{current.Prev} <=> ";
+                current = current.Prev;
+            }
+            return outpot;
+        }
+    }
+}
